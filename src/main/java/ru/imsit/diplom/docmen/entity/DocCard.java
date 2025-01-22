334183e775d3e4cf34629452b1a111a0c6daae04
Vadim <vadimka_smetanin@mail.ru>
2025-01-22 23:55:38 +0300

    add project settings

---
+++ src/main/java/ru/imsit/diplom/docmen/entity/DocCard.java
@@ -0,0 +1,32 @@
+package ru.imsit.diplom.docmen.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "doc_card")
+public class DocCard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @Column(name = "name")
+    @JdbcTypeCode(SqlTypes.VARCHAR)
+    private String name;
+
+    @Column(name = "discription")
+    @JdbcTypeCode(SqlTypes.VARCHAR)
+    private String discription;
+
+
+
+}
