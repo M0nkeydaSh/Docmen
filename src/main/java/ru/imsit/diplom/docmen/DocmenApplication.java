98abc8d0328eb53fde227d915b1516570d36d4e9
Vadim <vadimka_smetanin@mail.ru>
2025-01-22 22:15:00 +0300

    Initial commit

---
+++ src/main/java/ru/imsit/diplom/docmen/DocmenApplication.java
@@ -0,0 +1,13 @@
+package ru.imsit.diplom.docmen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocmenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocmenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/imsit/diplom/docmen/DocmenApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.imsit.diplom.docmen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocmenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
